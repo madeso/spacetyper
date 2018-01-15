b31d0b8a1ea8710a33ff0c6332ceff704a41cff5
Gustav <sir.gustav.the.coder@gmail.com>
2018-01-15 23:26:06 +0100

    fixed star order

--- spacetyper/background.cc
+++ spacetyper/background.cc
@@ -5,48 +5,64 @@
 #include "render/spriterender.h"
 #include "render/texture.h"
 
-namespace {
-std::mt19937 &Generator() {
-  static std::random_device rd;
-  static std::mt19937 gen(rd());
-  return gen;
-}
+namespace
+{
+  std::mt19937&
+  Generator()
+  {
+    static std::random_device rd;
+    static std::mt19937       gen(rd());
+    return gen;
+  }
 }
 
-std::uniform_real_distribution<float> GetDistribution(float window,
-                                                      float texture) {
-  const float d = texture / 2.0f;
-  const float minimum = 10;
-  std::uniform_real_distribution<float> rwidth(-d + minimum,
-                                               window + d - minimum);
+std::uniform_real_distribution<float>
+GetDistribution(float window, float texture)
+{
+  const float                           d       = texture / 2.0f;
+  const float                           minimum = 10;
+  std::uniform_real_distribution<float> rwidth(
+      -d + minimum, window + d - minimum);
   return rwidth;
 }
 
-Background::Background(int count, int width, int height,
-                       std::shared_ptr<Texture2d> texture, float speed,
-                       Layer *layer)
-    : width_(width), height_(height), speed_(speed) {
-  auto rwidth = GetDistribution(width, texture->GetWidth());
+Background::Background(
+    int                        count,
+    int                        width,
+    int                        height,
+    std::shared_ptr<Texture2d> texture,
+    float                      speed,
+    Layer*                     layer)
+    : width_(width)
+    , height_(height)
+    , speed_(speed)
+{
+  auto rwidth  = GetDistribution(width, texture->GetWidth());
   auto rheight = GetDistribution(height, texture->GetHeight());
 
   positions_.reserve(count);
-  for (int i = 0; i < count; ++i) {
-    vec2f p(rwidth(Generator()), rheight(Generator()));
+  for(int i = 0; i < count; ++i)
+  {
+    vec2f  p(rwidth(Generator()), rheight(Generator()));
     Sprite sp(texture, p);
     positions_.push_back(sp);
     layer->Add(&*positions_.rbegin());
   }
 }
 
-void Background::Update(float delta) {
-  for (Sprite &sp : positions_) {
+void
+Background::Update(float delta)
+{
+  for(Sprite& sp : positions_)
+  {
     vec2f p = sp.GetPosition();
-    p.y += delta * speed_;
+    p.y -= delta * speed_;
 
-    if (p.y > height_ + sp.GetHeight() / 2) {
+    if(p.y < -sp.GetHeight() / 2)
+    {
       auto rwidth = GetDistribution(width_, sp.GetWidth());
-      p.y = -sp.GetHeight() / 2;
-      p.x = rwidth(Generator());
+      p.y         = height_ + sp.GetHeight() / 2;
+      p.x         = rwidth(Generator());
     }
 
     sp.SetPosition(p);
